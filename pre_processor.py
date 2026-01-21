039eef3c59d7b6d2fa70db96d6be92c2419401f8
Viper2911 <aadilhasan29.5@gmail.com>
2026-01-21 22:22:21 +0530

    Update pre_processor.py

--- pre_processor.py
+++ pre_processor.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 def preprocess(df,region_df):
-
     df = df[df['Season'] == 'Summer']
     df = df.merge(region_df, on='NOC', how='left')
     df.drop_duplicates(inplace=True)
